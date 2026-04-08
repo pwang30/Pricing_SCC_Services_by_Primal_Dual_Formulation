edbe8a64290ec6b1492f88db83a384f76bdbc96d
peng wang <powerwang2017@gmail.com>
2026-04-08 16:44:45 +0200

    Update dataset_gene.jl

--- Primal-Dual-Formulation_pricing_SCC/dataset_gene.jl
+++ Primal-Dual-Formulation_pricing_SCC/dataset_gene.jl
@@ -5,12 +5,12 @@ function dataset_gene(I_IBG,β,v)
     num_IBG=3                           # number of nodes where IBGs are located
     #            Bus Number	     x1	     
         SGpara=[
-                   2	       0.0846		
-                   3	       0.0799		
-                   4	       0.0758		
-                   5	       0.0731		
-                   27	       0.0519		
-                   30	       0.0523 ]  # buses where SGs are located
+                   2	       0.0846	0.0812	
+                   3	       0.0799	0.0774	
+                   4	       0.0758	0.0735	
+                   5	       0.0731	0.0713	
+                   27	       0.0519	0.0457	
+                   30	       0.0523	0.0469 ]  # buses where SGs are located
 
     Y_SGs = zeros(size(SGpara,1), size(SGpara,2)-1)    # define ADMITTANCE MATRIX of the SGs, buses:2,3,4,5,27,30
     I_SGs = zeros(size(SGpara,1), size(SGpara,2)-1)    # define I_SGs of the SGs, buses:2,3,4,5,27,30
@@ -19,7 +19,7 @@ function dataset_gene(I_IBG,β,v)
 
     for k in 1:size(SGpara,1)                      # calculate the ADMITTANCE MATRIX of the SGs
         for j in 2:size(SGpara,2)
-            Y_SGs[k, j-1] = 1/SGpara[k, j]         # /10        
+            Y_SGs[k, j-1] = 1/SGpara[k, j]/10         # /10        
         end           
     end 
     
@@ -64,9 +64,13 @@ function dataset_gene(I_IBG,β,v)
             status_IBG = matrix_ω[k, length(SGpara[:,2:end])+1:length(SGpara[:,2:end]) + num_IBG]   # status of IBG
             Y_SGs_with_status .= 0                                                                  # reset Y_SGs_with_status matrix
         
-
+            index = 1
             for i in 1:length(SGpara[:,2:end])
-                Y_SGs_with_status[Int(SGpara[Int(ceil(i/2)), 1]), Int(SGpara[Int(ceil(i/2)), 1])] = Y_SGs_with_status[Int(SGpara[Int(ceil(i/2)), 1]), Int(SGpara[Int(ceil(i/2)), 1])] * status_SGs[i]  # status of SGs
+                Y_SGs_with_status[Int(SGpara[Int(ceil(i/2)), 1]), Int(SGpara[Int(ceil(i/2)), 1])] = Y_SGs_with_status[Int(SGpara[Int(ceil(i/2)), 1]), Int(SGpara[Int(ceil(i/2)), 1])]+Y_SGs[Int(ceil(i/2)),index] * status_SGs[i]  # status of SGs
+                    index = index+1
+                    if index > size(Y_SGs, 2)
+                        index = 1
+                    end
             end
         
             Y_total .= Yₗᵢₙₑ + Y_SGs_with_status      # calculate the total ADMITTANCE MATRIX
@@ -88,3 +92,4 @@ function dataset_gene(I_IBG,β,v)
     
     
 end
+
