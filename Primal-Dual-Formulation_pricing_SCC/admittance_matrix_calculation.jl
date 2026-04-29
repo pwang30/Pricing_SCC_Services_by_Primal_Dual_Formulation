5562a34d445f4458b551b8cfe463178ebc20125b
peng wang <powerwang2017@gmail.com>
2026-04-29 19:12:51 +0200

    Update admittance_matrix_calculation.jl

--- Primal-Dual-Formulation_pricing_SCC/admittance_matrix_calculation.jl
+++ Primal-Dual-Formulation_pricing_SCC/admittance_matrix_calculation.jl
@@ -1,14 +1,13 @@
 function admittance_matrix_calculation(numnodes)
     df = DataFrame(CSV.File( "/Users/kl/Desktop/SCL_pricing_PD/Linespara.csv" )) 
- linepara=df[:,:]
-
+    linepara=df[:,:]
+    
     Yₗᵢₙₑ= zeros(numnodes, numnodes)         # define admittance matrix of the transmission lines
     branch_num= size(linepara, 1)           # number of branches
     for k in 1:branch_num                   #  calculate the ADMITTANCE MATRIX of the transmission lines
         i = linepara[k, 1]                  # bus from
         j = linepara[k, 2]                  # bus to
- #       Yₗᵢₙₑ[i, j] = -1/linepara[k, 4]*3    # off-diagonal elements
-        Yₗᵢₙₑ[i, j] = -1/linepara[k, 4]  
+        Yₗᵢₙₑ[i, j] = -1/linepara[k, 4]*3    # off-diagonal elements *3
         Yₗᵢₙₑ[j, i] = Yₗᵢₙₑ[i, j]             # symmetry
     end
     for k in 1:numnodes
