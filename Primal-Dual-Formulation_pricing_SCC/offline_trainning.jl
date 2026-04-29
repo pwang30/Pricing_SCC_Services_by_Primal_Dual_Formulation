6225251906863b6a2d8293e14b19eafa1e7d6481
peng wang <powerwang2017@gmail.com>
2026-04-29 19:12:11 +0200

    Update offline_trainning.jl

--- Primal-Dual-Formulation_pricing_SCC/offline_trainning.jl
+++ Primal-Dual-Formulation_pricing_SCC/offline_trainning.jl
@@ -1,129 +1,158 @@
-function offline_trainning(zᵟ_c, matrix_ω)
+function offline_trainning(I_SCC_all_buses_scenarios, matrix_ω, Iₗᵢₘ, v)
 
-    a=6
-    b=15
 #-----------------------------------Dataset Classification
-    K_c_gc=zeros(2,a)   # define the linearized coffecoents for SGs
-    K_c_gv=zeros(2,1)    # define the linearized coffecoents for VSGs
-    K_c_m=zeros(2,a+b)   # define the linearized coffecoents for interactions of each two (V)SGs.   78=66+12
+    K_g=zeros(12,30)   # define the linearized coffecoents for SGs
+    K_c=zeros(3,30)    # define the linearized coffecoents for IBGs
+    K_m=zeros(66,30)  # define the linearized coffecoents for pairs of SGs    C(12,2) = 66
 
+    N_Ω1_tp1=0  # define the number of type-I & II errors 
+    N_Ω2_tp1=0
+    N_Ω3_tp1=0
+    N_Ω1_tp2=0
+    N_Ω2_tp2=0
+    N_Ω3_tp2=0
 
-    #=
-#-----------------------------------Define Offline-Training Model.  z₂₃²⁴
-        Training_z₂₃²⁴= Model()
-        #  @variable(Training_z₂₃²⁴, K_z₂₃²⁴[1:3])  # shape control
-        @variable(Training_z₂₃²⁴, sh_1[1])
-        @variable(Training_z₂₃²⁴, K_23_gc[1:a])  # buses of SGs are 2,3,4,5,27,30.
-        @variable(Training_z₂₃²⁴, K_23_gv[1:1])   # buses of VSGs are 1,23,24.
-        @variable(Training_z₂₃²⁴, K_23_m[1:a+b])   # pairs of (V)SGs
-        @variable(Training_z₂₃²⁴, z_23_24[1:size(zᵟ_c,1)])   
-        @variable(Training_z₂₃²⁴, penalty_z₂₃²⁴[1:size(zᵟ_c,1)])
+    err_Ω1_tp1=0  # define the error of type-I & II
+    err_Ω2_tp1=0
+    err_Ω3_tp1=0
+    err_Ω1_tp2=0
+    err_Ω2_tp2=0
+    err_Ω3_tp2=0
 
-        for i in 1:size(zᵟ_c,1)
-            #@constraint(Training_z₂₃²⁴, z_23_24[i]==K_z₂₃²⁴[1]*(sum(K_23_gc .*matrix_ω[i,1:a]) +K_z₂₃²⁴[2]*sum(K_23_gv *matrix_ω[i,a+1]) +K_z₂₃²⁴[3]*sum(K_23_m .*matrix_ω[i,a+2:end])))
-             @constraint(Training_z₂₃²⁴, z_23_24[i]==sh_1[1]-(sum(K_23_gc .*matrix_ω[i,1:a]) +sum(K_23_gv *matrix_ω[i,a+1]) +sum(K_23_m .*matrix_ω[i,a+2:end])))
-           
-            
-            @constraint(Training_z₂₃²⁴, penalty_z₂₃²⁴[i]==z_23_24[i] -zᵟ_c[i,1])
+    for k in 1:size(I_SCC_all_buses_scenarios,2)
+    
+        I_scc_Ω1 = []     # initialize the classification matrix
+        I_scc_Ω2 = []
+        I_scc_Ω3 = []
+        matrix_ω_Ω1 = []
+        matrix_ω_Ω2 = []
+        matrix_ω_Ω3 = []
+    
+        for i in 1:size(I_SCC_all_buses_scenarios,1)  # traverse I_scc and matrix_ω for classification
+            if I_SCC_all_buses_scenarios[i,k] < Iₗᵢₘ
+                push!(I_scc_Ω1, I_SCC_all_buses_scenarios[i,k])
+                push!(matrix_ω_Ω1, matrix_ω[i,:])
+            elseif Iₗᵢₘ <= I_SCC_all_buses_scenarios[i,k] < Iₗᵢₘ + v
+                push!(I_scc_Ω2, I_SCC_all_buses_scenarios[i,k])
+                push!(matrix_ω_Ω2, matrix_ω[i,:])
+            elseif I_SCC_all_buses_scenarios[i,k] >= Iₗᵢₘ + v
+                push!(I_scc_Ω3, I_SCC_all_buses_scenarios[i,k])
+                push!(matrix_ω_Ω3, matrix_ω[i,:])
+            end
         end
+    
+        I_scc_Ω1 = hcat(I_scc_Ω1...)'               # convert the classification results into a matrix
+        I_scc_Ω2 = hcat(I_scc_Ω2...)'
+        I_scc_Ω3 = hcat(I_scc_Ω3...)'
+        matrix_ω_Ω1 = hcat(matrix_ω_Ω1...)'
+        matrix_ω_Ω2 = hcat(matrix_ω_Ω2...)'
+        matrix_ω_Ω3 = hcat(matrix_ω_Ω3...)'
+    
+    
+#-----------------------------------Define Offline-Training Model
+        model_ot= Model()
+    
+        @variable(model_ot, k_fg[1:12])  # buses of SGs are 2,3,4,5,27,30.
+        @variable(model_ot, k_fc[1:3])  # buses of IBG are 1,23,26. 
+        @variable(model_ot, k_fm[1:66]) # pairs of SGs
+    
+        @variable(model_ot, I_FL_ω_Ω1[1:size(matrix_ω_Ω1,1)])  
+        @variable(model_ot, I_FL_ω_Ω2[1:size(matrix_ω_Ω2,1)])  
+        @variable(model_ot, I_FL_ω_Ω3[1:size(matrix_ω_Ω3,1)])  
+    
+        for i in 1:size(matrix_ω_Ω1,1)
+            @constraint(model_ot, I_FL_ω_Ω1[i]==sum(k_fg.* matrix_ω_Ω1[i,1:12])+ sum(k_fc.* matrix_ω_Ω1[i,13:15])+ sum(k_fm.* matrix_ω_Ω1[i,16:81]))
+            @constraint(model_ot, I_FL_ω_Ω1[i]<=Iₗᵢₘ ) 
+        end
+    
+        for i in 1:size(matrix_ω_Ω3,1)
+            @constraint(model_ot, I_FL_ω_Ω3[i]==sum(k_fg.* matrix_ω_Ω3[i,1:12])+ sum(k_fc.* matrix_ω_Ω3[i,13:15])+ sum(k_fm.* matrix_ω_Ω3[i,16:81]))
+            @constraint(model_ot, I_FL_ω_Ω3[i]>=Iₗᵢₘ ) 
+        end
+    
+        @variable(model_ot, penalty_one_time_item[1:size(matrix_ω_Ω2,1)])
+        for i in 1:size(matrix_ω_Ω2,1)
+            @constraint(model_ot, I_FL_ω_Ω2[i]==sum(k_fg.* matrix_ω_Ω2[i,1:12])+ sum(k_fc.* matrix_ω_Ω2[i,13:15])+ sum(k_fm.* matrix_ω_Ω2[i,16:81]))
+            @constraint(model_ot, penalty_one_time_item[i]==I_scc_Ω2[i]- I_FL_ω_Ω2[i])
+        end
+    
+    
+        @objective(model_ot, Min, sum(penalty_one_time_item.^2))  
 
-        @objective(Training_z₂₃²⁴, Min, sum(penalty_z₂₃²⁴.^2))  
 
-        set_optimizer(Training_z₂₃²⁴, Gurobi.Optimizer)
-        optimize!(Training_z₂₃²⁴)
+#-----------------------------------Solve and Output Results
+        set_optimizer(model_ot , Gurobi.Optimizer)
+        @time optimize!(model_ot)
+        optimize!(model_ot)
     
 
+        k_fg=JuMP.value.(k_fg)
+        k_fc=JuMP.value.(k_fc)
+        k_fm=JuMP.value.(k_fm)
+ 
+        K_g[:,k]=k_fg'
+        K_c[:,k]=k_fc'
+        K_m[:,k]=k_fm'
+    
+        I_FL_ω_Ω1=JuMP.value.(I_FL_ω_Ω1)
+        I_FL_ω_Ω2=JuMP.value.(I_FL_ω_Ω2)
+        I_FL_ω_Ω3=JuMP.value.(I_FL_ω_Ω3)
 
-#-----------------------------------Define Offline-Training Model.  z₂₄²³
-        Training_z₂₄²³= Model()
-        #  @variable(Training_z₂₄²³, K_z₂₄²³[1:3])  # shape control
-        @variable(Training_z₂₄²³, sh_2[1])
-        @variable(Training_z₂₄²³, K_24_gc[1:a])  # buses of SGs are 2,3,4,5,27,30.
-        @variable(Training_z₂₄²³, K_24_gv[1:1])   # buses of VSGs are 1,23,24.
-        @variable(Training_z₂₄²³, K_24_m[1:a+b])   # pairs of (V)SGs
-        @variable(Training_z₂₄²³, z_24_23[1:size(zᵟ_c,1)])   
-        @variable(Training_z₂₄²³, penalty_z₂₄²³[1:size(zᵟ_c,1)])
 
-        for i in 1:size(zᵟ_c,1)
-          #    @constraint(Training_z₂₄²³, z_24_23[i]==K_z₂₄²³[1]*(sum(K_24_gc .*matrix_ω[i,1:a]) +K_z₂₄²³[2]*sum(K_24_gv *matrix_ω[i,a+1]) +K_z₂₄²³[3]*sum(K_24_m .*matrix_ω[i,a+2:end])))
-           @constraint(Training_z₂₄²³, z_24_23[i]==sh_2[1]-(sum(K_24_gc .*matrix_ω[i,1:a]) +sum(K_24_gv *matrix_ω[i,a+1]) +sum(K_24_m .*matrix_ω[i,a+2:end])))
-          @constraint(Training_z₂₄²³, penalty_z₂₄²³[i]==z_24_23[i] -zᵟ_c[i,2])
+#-----------------------------------Calculate errors for type-1
+        for  i in 1:size(I_FL_ω_Ω1,2) # error calculation
+            if I_FL_ω_Ω1[i] >= Iₗᵢₘ &&  I_scc_Ω1[i] < Iₗᵢₘ
+                N_Ω1_tp1=N_Ω1_tp1+1
+                err_Ω1_tp1= err_Ω1_tp1+(I_FL_ω_Ω1[i] - I_scc_Ω1[i])/I_scc_Ω1[i]
+            end
+        end
+    
+        for  i in 1:size(I_FL_ω_Ω2,2) # error calculation
+            if I_FL_ω_Ω2[i] >= Iₗᵢₘ &&  I_scc_Ω2[i] < Iₗᵢₘ
+                N_Ω2_tp1=N_Ω2_tp1+1
+                err_Ω2_tp1= err_Ω2_tp1+(I_FL_ω_Ω2[i] - I_scc_Ω2[i])/I_scc_Ω2[i]
+            end
+        end
+    
+        for  i in 1:size(I_FL_ω_Ω3,2) # error calculation
+            if I_FL_ω_Ω3[i] >= Iₗᵢₘ &&  I_scc_Ω3[i] < Iₗᵢₘ
+                N_Ω3_tp1=N_Ω3_tp1+1
+                err_Ω3_tp1= err_Ω3_tp1+(I_FL_ω_Ω3[i] - I_scc_Ω3[i])/I_scc_Ω3[i]
+            end
+        end
+    
+
+#-----------------------------------Calculate errors for type-2
+    
+        for  i in 1:size(I_FL_ω_Ω1,2) # error calculation
+            if I_FL_ω_Ω1[i] < Iₗᵢₘ &&  I_scc_Ω1[i] >= Iₗᵢₘ
+                N_Ω1_tp2=N_Ω1_tp2+1
+                err_Ω1_tp2= err_Ω1_tp2+(I_FL_ω_Ω1[i] - I_scc_Ω1[i])/I_scc_Ω1[i]
+            end
         end
 
-        @objective(Training_z₂₄²³, Min, sum(penalty_z₂₄²³.^2))
-
-        set_optimizer(Training_z₂₄²³, Gurobi.Optimizer)
-        optimize!(Training_z₂₄²³)
-
-
-  =#
-
-#-----------------------------------Define Offline-Training Model.  z₂₃¹
-        Training_z₂₃¹= Model()
-
-        @variable(Training_z₂₃¹, K_23_1_gc[1:a])  # buses of SGs are 2,3,4,5,27,30.
-        @variable(Training_z₂₃¹, K_23_1_gv[1:1])   # buses of VSGs are 1,23,24.
-        @variable(Training_z₂₃¹, K_23_1_m[1:a+b])   # pairs of (V)SGs
-        @variable(Training_z₂₃¹, z_23_1[1:size(zᵟ_c,1)])   
-        @variable(Training_z₂₃¹, penalty_z₂₃¹[1:size(zᵟ_c,1)])
-
-        for i in 1:size(zᵟ_c,1)
-            @constraint(Training_z₂₃¹, z_23_1[i]==(sum(K_23_1_gc .*matrix_ω[i,1:a]) +sum(K_23_1_gv *matrix_ω[i,a+1]) +sum(K_23_1_m .*matrix_ω[i,a+2:end])))
-            @constraint(Training_z₂₃¹, penalty_z₂₃¹[i]==z_23_1[i] -zᵟ_c[i,1])
+        for  i in 1:size(I_FL_ω_Ω2,2) # error calculation
+            if I_FL_ω_Ω2[i] < Iₗᵢₘ &&  I_scc_Ω2[i] >= Iₗᵢₘ
+                N_Ω2_tp2=N_Ω2_tp2+1
+                err_Ω2_tp2= err_Ω2_tp2+(I_FL_ω_Ω2[i] - I_scc_Ω2[i])/I_scc_Ω2[i]
+            end
         end
 
-        @objective(Training_z₂₃¹, Min, sum(penalty_z₂₃¹.^2))
-
-        set_optimizer(Training_z₂₃¹, Gurobi.Optimizer)
-        optimize!(Training_z₂₃¹)
-
-
-
-#-----------------------------------Define Offline-Training Model.  z₂₄¹
-        Training_z₂₄¹= Model()
-
-        @variable(Training_z₂₄¹, K_24_1_gc[1:a])  # buses of SGs are 2,3,4,5,27,30.
-        @variable(Training_z₂₄¹, K_24_1_gv[1:1])   # buses of VSGs are 1,23,26.
-        @variable(Training_z₂₄¹, K_24_1_m[1:a+b])   # pairs of SGs
-        @variable(Training_z₂₄¹, z_24_1[1:size(zᵟ_c,1)])   
-        @variable(Training_z₂₄¹, penalty_z₂₄¹[1:size(zᵟ_c,1)])
-
-        for i in 1:size(zᵟ_c,1)
-            @constraint(Training_z₂₄¹, z_24_1[i]==(sum(K_24_1_gc .*matrix_ω[i,1:a]) +sum(K_24_1_gv *matrix_ω[i,a+1]) +sum(K_24_1_m .*matrix_ω[i,a+2:end])))
-            @constraint(Training_z₂₄¹, penalty_z₂₄¹[i]==z_24_1[i] -zᵟ_c[i,2])
+        for  i in 1:size(I_FL_ω_Ω3,2) # error calculation
+            if I_FL_ω_Ω3[i] < Iₗᵢₘ &&  I_scc_Ω3[i] >= Iₗᵢₘ
+                N_Ω3_tp2=N_Ω3_tp2+1
+                err_Ω3_tp2= err_Ω3_tp2+(I_FL_ω_Ω3[i] - I_scc_Ω3[i])/I_scc_Ω3[i]
+            end
         end
 
-        @objective(Training_z₂₄¹, Min, sum(penalty_z₂₄¹.^2))
-
-        set_optimizer(Training_z₂₄¹, Gurobi.Optimizer)
-        optimize!(Training_z₂₄¹)
+    end
 
 
-K_23_1_gc = value.(K_23_1_gc)
-K_24_1_gc = value.(K_24_1_gc)
-K_23_1_gv = value.(K_23_1_gv)
-K_24_1_gv = value.(K_24_1_gv)
-K_23_1_m = value.(K_23_1_m)
-K_24_1_m = value.(K_24_1_m)
-#sh_1 = value.(sh_1[1])
-#sh_2 = value.(sh_2[1])
-#shape_control = zeros(2,3)
-#shape_control[1,:] = value.(K_z₂₃²⁴)
-#shape_control[2,:] = value.(K_z₂₄²³)
-K_c_gc[1,:]=K_23_1_gc
-K_c_gc[2,:]=K_24_1_gc
+    N_type_1=N_Ω1_tp1+N_Ω2_tp1+N_Ω3_tp1    
+    err_type_1=(err_Ω1_tp1+err_Ω2_tp1+err_Ω3_tp1)/N_type_1
 
-K_c_gv[1,:]=K_23_1_gv
-K_c_gv[2,:]=K_24_1_gv
+    N_type_2=N_Ω1_tp2+N_Ω2_tp2+N_Ω3_tp2    
+    err_type_2=(err_Ω1_tp2+err_Ω2_tp2+err_Ω3_tp2)/N_type_2
 
-K_c_m[1,:]=K_23_1_m
-K_c_m[2,:]=K_24_1_m
-
-z_23_1 = value.(z_23_1)
-z_24_1 = value.(z_24_1)
-# MAPE calculation
-MAPE_z_23_1 = sum(abs.(z_23_1 - zᵟ_c[:,1]) ./ abs.(zᵟ_c[:,1])) / size(zᵟ_c,1)
-MAPE_z_24_1 = sum(abs.(z_24_1 - zᵟ_c[:,2]) ./ abs.(zᵟ_c[:,2])) / size(zᵟ_c,1)
-
-    return  K_c_gc, K_c_gv, K_c_m, MAPE_z_23_1, MAPE_z_24_1 #shape_control , sh_1, sh_2
+    return  K_g, K_c, K_m, N_type_1, N_type_2, err_type_1, err_type_2
 end
